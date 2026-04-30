1e5de1f9032a9d6e42b5b444b5ad63a1d97b0384
Hersyanda Putra Adi <49233604+rlmyandaa@users.noreply.github.com>
2026-04-30 19:42:31 +0700

    patch: cleanup

--- models/ir_http.py
+++
@@ -1,6 +0,0 @@
-from odoo import models
-
-class IrHttp(models.AbstractModel):
-    _inherit = 'ir.http'
-    # Simple inheritance to ensure the module is seen as having python logic
-    # and to force Odoo to recognize it during 'Update Apps List'
